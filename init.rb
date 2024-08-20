025cbe5637f692070c43b2ab08c220b797275f3d
Markus Boremski <markus@boremski.net>
2024-08-20 15:34:02 +0200

    Update init.rb for Zeitwerk

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_regexidentifier do
   name 'Redmine Regex Identifier Plugin'
   author 'Markus Boremski'
   description 'This plugin checks the project identifier against a regex.'
-  version '0.0.2'
+  version '0.0.3'
   url 'https://github.com/mboremski/redmine_regexidentifier'
   author_url 'https://github.com/mboremski'
   settings default: {'regex' => '\Aed[0-9]*\z', 'enabled' => true}, partial: 'settings/redmine_regexidentifier_settings'
@@ -12,8 +12,7 @@ end
 
 Rails.configuration.to_prepare do
   require_dependency 'project'
-  require_dependency 'redmine_regexidentifier/project_patch'
-  unless Project.included_modules.include?(RedmineRegexIdentifier::ProjectPatch)
-    Project.send(:include, RedmineRegexIdentifier::ProjectPatch)
-  end
+  require_dependency File.expand_path('../lib/redmine_regexidentifier/project_patch', __FILE__)
+
+  Project.include RedmineRegexidentifier::ProjectPatch
 end
