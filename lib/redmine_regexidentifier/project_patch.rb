be3b1b0e37cc8efcbc6bb259365448844466cc9d
Markus Boremski <markus@boremski.net>
2024-08-20 15:34:35 +0200

    Update project_patch.rb for Zeitwerk

--- lib/redmine_regexidentifier/project_patch.rb
+++ lib/redmine_regexidentifier/project_patch.rb
@@ -1,32 +1,20 @@
-require_dependency 'project'
-
-module RedmineRegexIdentifier
+module RedmineRegexidentifier
   module ProjectPatch
     def self.included(base)
-      base.extend(ClassMethods)
-      base.send(:include, InstanceMethods)
       base.class_eval do
-        unloadable
         validate :validate_identifier_regex, on: :create
       end
     end
 
-    module ClassMethods
-    end
-
-    module InstanceMethods
-      def validate_identifier_regex
-        if Setting.plugin_redmine_regexidentifier['enabled']
-          regex_string = Setting.plugin_redmine_regexidentifier['regex']
-          regex_string_without_linebreaks = regex_string.gsub(/[\r\n]+/, '')
-          regex = Regexp.new(regex_string_without_linebreaks)
-          unless regex.match(identifier)
-            errors.add(:identifier, :invalid)
-          end
+    def validate_identifier_regex
+      if Setting.plugin_redmine_regexidentifier['enabled']
+        regex_string = Setting.plugin_redmine_regexidentifier['regex']
+        regex_string_without_linebreaks = regex_string.gsub(/[\r\n]+/, '')
+        regex = Regexp.new(regex_string_without_linebreaks)
+        unless regex.match(identifier)
+          errors.add(:identifier, :invalid)
         end
       end
     end
   end
 end
-
-Project.send(:include, RedmineRegexIdentifier::ProjectPatch)
