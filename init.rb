771a84bfdb65ddff906c17aa12e88b0191f7b962
Bekhruz Nikizad <Bekhruz.Nikizad@mentor-bauelemente.de>
2026-02-09 16:14:44 +0100

    hotfix

--- init.rb
+++ init.rb
@@ -1,5 +1,6 @@
 require 'redmine'
 
+# 1. Plugin Registrierung
 Redmine::Plugin.register :redmine_regexidentifier do
   name 'Redmine Regex Identifier Plugin'
   author 'Markus Boremski'
@@ -7,12 +8,18 @@ Redmine::Plugin.register :redmine_regexidentifier do
   version '0.0.3'
   url 'https://github.com/mboremski/redmine_regexidentifier'
   author_url 'https://github.com/mboremski'
-  settings default: {'regex' => '\Aed[0-9]*\z', 'enabled' => true}, partial: 'settings/redmine_regexidentifier_settings'
+  settings default: {'regex' => '\Aed[0-9]*\z', 'enabled' => true}, 
+           partial: 'settings/redmine_regexidentifier_settings'
 end
 
-Rails.configuration.to_prepare do
-  require_dependency 'project'
-  require_dependency File.expand_path('../lib/redmine_regexidentifier/project_patch', __FILE__)
+# 2. Patch sofort erzwingen
+require 'project'
 
-  Project.include RedmineRegexidentifier::ProjectPatch
+require_dependency File.expand_path('../lib/redmine_regexidentifier/project_patch', __FILE__)
+unless Project.included_modules.include?(RedmineRegexidentifier::ProjectPatch)
+  Project.send(:include, RedmineRegexidentifier::ProjectPatch)
+  # Diese Zeile MUSST du in den Docker-Logs sehen
+  puts "================================================"
+  puts "!!! REGEX-PLUGIN: Patch wurde geladen !!!"
+  puts "================================================"
 end
--- lib/redmine_regexidentifier/project_patch.rb
+++ lib/redmine_regexidentifier/project_patch.rb
@@ -2,17 +2,32 @@ module RedmineRegexidentifier
   module ProjectPatch
     def self.included(base)
       base.class_eval do
+        # Die Validierung soll beim erstellen prüfen
         validate :validate_identifier_regex, on: :create
       end
     end
 
     def validate_identifier_regex
-      if Setting.plugin_redmine_regexidentifier['enabled']
+      # 1. Prüfen, ob das Plugin in den Einstellungen aktiviert ist
+      if Setting.plugin_redmine_regexidentifier && Setting.plugin_redmine_regexidentifier['enabled']
+        
         regex_string = Setting.plugin_redmine_regexidentifier['regex']
-        regex_string_without_linebreaks = regex_string.gsub(/[\r\n]+/, '')
-        regex = Regexp.new(regex_string_without_linebreaks)
-        unless regex.match(identifier)
-          errors.add(:identifier, :invalid)
+        
+        # Falls kein Regex eingetragen wurde, nichts tun
+        return if regex_string.blank?
+
+        begin
+          # Regex säubern (Linebreaks entfernen) und initialisieren
+          regex = Regexp.new(regex_string.gsub(/[\r\n]+/, ''))
+          
+          # 2. Den Identifier gegen die Regex prüfen
+          unless regex.match(identifier)
+            # Fehlermeldung ausgeben, wenn es nicht passt
+            errors.add(:identifier, "entspricht nicht dem erforderlichen Muster (#{regex_string})")
+          end
+        rescue RegexpError => e
+          # Falls die eingegebene Regex im Admin-Bereich ungültig ist
+          errors.add(:identifier, "konnte nicht geprüft werden, da die Regex ungültig ist.")
         end
       end
     end
